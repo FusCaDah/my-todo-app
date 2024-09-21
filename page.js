e89aceaa9e36508da9d9cec93d521bf379ba8f6d
Ziricx <85381847+FusCaDah@users.noreply.github.com>
2024-09-20 23:08:30 -0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'; // Adjust the path if necessary
+import HomeTarefas from './components/HomeTarefas';
+
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <HomeTarefas />
+    </>
+  );
+}
